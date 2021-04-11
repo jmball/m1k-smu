9c98ef33847d3e138a29f8c1a90f338663526e39
jmball <jmball@users.noreply.github.com>
2021-04-11 22:10:12 +0100

    updated sweep example accounting for fixes to m1k

--- examples/sweep.py
+++ examples/sweep.py
@@ -1,8 +1,13 @@
 """Example using the m1k library to perform a voltage sweep."""
 
+import pathlib
+import sys
+
+sys.path.insert(1, str(pathlib.Path.cwd().parent))
+
 import matplotlib.pyplot as plt
 
-from . import m1k
+import m1k.m1k as m1k
 
 
 with m1k.smu() as smu:
@@ -11,41 +16,25 @@ with m1k.smu() as smu:
 
     # configure all outputs
     smu.configure_all_channels(
-        nplc=0.2, settling_delay=0.005, auto_off=False, four_wire=True, v_range=5
+        nplc=1, settling_delay=0.005, auto_off=False, four_wire=True, v_range=5
     )
 
-    # configure a sweep for channel 0
-    smu.configure_sweep(
-        start=0, stop=0.6, points=13, dual=True, source_mode="v", channel=0
-    )
+    # configure a sweep
+    smu.configure_sweep(start=0, stop=0.6, points=13, dual=True, source_mode="v")
 
-    # measure the sweep on channel 0, accouting for nplc and settling delay
-    data = smu.measure(channel=0, process=True)
+    # measure the sweep
+    data = smu.measure("sweep")
 
-    # disconnect all devices
-    smu.disconnect()
+    # disable output manually because auto-off is false
+    smu.enable_output(False)
 
-# extract processed and raw data for plotting
-voltages_p = [v for v, i, t, s in data["processed"]]
-currents_p = [i * 1000 for v, i, t, s in data["processed"]]
-
-times_r = [t for v, i, t, s in data["raw"]]
-voltages_r = [v for v, i, t, s in data["raw"]]
-currents_r = [i * 1000 for v, i, t, s in data["raw"]]
+# extract data for plotting
+voltages_p = [v for v, i, t, s in data[0]]
+currents_p = [i * 1000 for v, i, t, s in data[0]]
 
 # plot the processed data
 fig, ax = plt.subplots()
-ax.plot(voltages_p, currents_p)
-ax.axhline(0, lw=0.5)
-ax.tick_params(direction="in", top=True, right=True, labelsize="large")
-ax.set_xlabel("Applied bias (V)", fontsize="large")
-ax.set_ylabel("Current (mA)", fontsize="large")
-
-fig.show()
-
-# plot the raw data
-fig, ax = plt.subplots()
-ax.plot(voltages_p, currents_p)
+ax.scatter(voltages_p, currents_p)
 ax.axhline(0, lw=0.5)
 ax.tick_params(direction="in", top=True, right=True, labelsize="large")
 ax.set_xlabel("Applied bias (V)", fontsize="large")
