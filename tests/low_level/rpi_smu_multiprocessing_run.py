5a1afdaf300a71d4582bf329c2e76b28dec5cea8
jmball <jmball@users.noreply.github.com>
2021-05-11 23:04:09 +0100

    added comments

--- tests/low_level/rpi_smu_multiprocessing_run.py
+++ tests/low_level/rpi_smu_multiprocessing_run.py
@@ -3,7 +3,9 @@ import time
 
 import pysmu
 
+# create session
 s = pysmu.Session()
+print(f"Number of devices: {len(s.devices)}\n")
 
 
 def write_all(v):
@@ -14,42 +16,56 @@ def write_all(v):
         dev.channels["A"].mode = pysmu.Mode.SVMI
 
 
+def run(s, n):
+    s.run(n)
+
+
 if __name__ == "__main__":
+    # write voltage
     t0 = time.time()
     write_all(0)
     t1 = time.time()
-    print(f"write time: {t1-t0}s")
+    print(f"write time: {t1-t0}s\n")
 
+    # number of samples
     n = 100000
 
+    # run capture
     failed = True
     i = 0
-    while (failed is True) or (i > 3):
+    while (failed is True) or (i < 3):
+        print(f"Run attempt {i}...")
         t2 = time.time()
 
-        p = multiprocessing.Process(target=s.run, args=(n,))
+        p = multiprocessing.Process(
+            target=run,
+            args=(
+                s,
+                n,
+            ),
+        )
         p.start()
 
         # 5s timeout for run process
         p.join(5)
 
+        # if timeout occurs terminate process and try again
         if p.is_alive():
-            print(f"Attempt {i}: run failed!")
+            print("...run failed!\n")
 
             p.terminate()
             p.join()
         else:
-            print(f"Attempt {i}: run succeeded!")
+            print("...run succeeded!\n")
             failed = False
 
         t3 = time.time()
-
-        print(f"run time: {t3-t2}s")
+        print(f"run time: {t3-t2}s\n")
 
         i += 1
 
     t4 = time.time()
     data = s.read(n)
     t5 = time.time()
-    print(f"read time: {t5-t4}")
+    print(f"read time: {t5-t4}\n")
     print(len(data), [len(d) for d in data])
