a4d4c9331cb178f08d658d8c69518c03bd75fd13
jmball <jmball@users.noreply.github.com>
2021-05-12 14:58:47 +0100

    added lower level read

--- tests/low_level/rpi_smu_continuous_test_v2.py
+++ tests/low_level/rpi_smu_continuous_test_v2.py
@@ -42,20 +42,23 @@ vs = [0, 0.5, 1, 1.5]
 for v in vs:
     print(f"Voltage: {v}")
     t0 = time.time()
-    for dev in devs:
+    for ix, dev in enumerate(devs):
         dev.channels["A"].write([v], cyclic=True)
     t1 = time.time()
     print(f"write time: {t1-t0} s")
-    s.read(100000, -1)
-    data = s.read(2500, -1)
+    for ix, dev in enumerate(devs):
+        dev.read(100000, -1)
     t2 = time.time()
-    print(f"read time: {t2-t1} s")
+    print(f"dummy read time: {t2-t1} s")
+    data = s.read(2500, -1)
+    t3 = time.time()
+    print(f"read time: {t3-t2} s")
     print(f"Data lengths: {[len(d) for d in data]}")
     # removing the variable from memory at creating it again is faster than
     # overwriting
     del data
-    t3 = time.time()
-    print(f"Del time: {t3-t2} s\n")
+    t4 = time.time()
+    print(f"Del time: {t4-t3} s\n")
 
 # end continuous mode
 if os.name == "nt":
