dca2ab626d460467bfdba991ad72213496e56f9b
jmball <jmball@users.noreply.github.com>
2021-05-13 21:23:07 +0100

    added scans var

--- tests/low_level/rpi_smu_run_time_test.py
+++ tests/low_level/rpi_smu_run_time_test.py
@@ -29,11 +29,12 @@ def write_all(v, retries=3):
 if __name__ == "__main__":
     retries = 10
     n = 10000
+    scans = 2500
 
     expected_lengths = [n] * len(s.devices)
     total_times = []
     dropped_scans = []
-    for i in range(500):
+    for i in range(scans):
         print(f"Scan {i}")
         t0 = time.time()
         write_all(0, retries)
