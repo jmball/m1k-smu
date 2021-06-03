df01924722c87882f4cfa952234027f7cedec4c0
jmball <jmball@users.noreply.github.com>
2021-06-03 18:18:24 +0100

    added program to visually correlate boards with serial numbers

---
+++ examples/identify_boards.py
@@ -0,0 +1,21 @@
+"""Identify board serial numbers by lighting their LEDs."""
+
+import pathlib
+import sys
+
+sys.path.insert(1, str(pathlib.Path.cwd().parent))
+import m1k.m1k as m1k
+
+
+with m1k.smu() as smu:
+    smu.connect()
+
+    for board in smu.num_boards:
+        smu.set_leds(2 * board, R=True, G=True, B=True)
+
+        input(
+            f"This board's serial is '{smu.get_channel_id(2 * board)}'. Press Enter to"
+            + " continue..."
+        )
+
+        smu.set_leds(2 * board, R=False, G=True, B=False)
