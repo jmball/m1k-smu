e93f2310f0c445326017bb33e08f03005f2993a4
jmball <jmball@users.noreply.github.com>
2021-06-03 18:01:43 +0100

    added psu and dmm imports

--- examples/calibration/measure_calibration_rigol.py
+++ examples/calibration/measure_calibration_rigol.py
@@ -6,9 +6,11 @@ import time
 import sys
 
 import numpy as np
-import pysmu
 import pyvisa
 import yaml
+
+import dp800
+import dm3058
 
 sys.path.insert(1, str(pathlib.Path.cwd().parent))
 import m1k.m1k as m1k
@@ -75,27 +77,35 @@ rigol_baud = args.rigol_baud
 rigol_flow_control = flow_controls[args.rigol_flow]
 rigol_term_char = term_chars[args.rigol_term]
 
-dmm = rm.open_resource(
+dmm = dm3058()
+dmm.connect(
     dmm_address,
-    baud_rate=rigol_baud,
-    flow_control=rigol_flow_control,
-    write_termination=rigol_term_char,
-    read_termination=rigol_term_char,
+    reset=True,
+    **{
+        "baud_rate": rigol_baud,
+        "flow_control": rigol_flow_control,
+        "write_termination": rigol_term_char,
+        "read_termination": rigol_term_char,
+    },
 )
-print(f"Rigol DM3058E ID: {dmm.query('*IDN?')}")
+print(f"Rigol DM3058E ID: {dmm.get_id()}")
 print("Connected!")
 
 print("\nConnecting to Rigol DP821A PSU...")
 psu_address = args.psu_address
 
-psu = rm.open_resource(
+psu = dp800()
+psu.connect(
     psu_address,
-    baud_rate=rigol_baud,
-    flow_control=rigol_flow_control,
-    write_termination=rigol_term_char,
-    read_termination=rigol_term_char,
+    reset=True,
+    **{
+        "baud_rate": rigol_baud,
+        "flow_control": rigol_flow_control,
+        "write_termination": rigol_term_char,
+        "read_termination": rigol_term_char,
+    },
 )
-print(f"Rigol DP821A ID: {psu.query('*IDN?')}")
+print(f"Rigol DP821A ID: {psu.get_id()}")
 print("Connected!")
 
 # connect to m1k's
@@ -125,14 +135,14 @@ smu.nplc = nplc
 smu.settling_delay = settling_delay
 
 for board in range(smu.num_boards):
-    print(f"SMU board {2 * board} ID: {smu.get_channel_id(2 * board)}")
+    print(f"SMU board {board} ID: {smu.get_channel_id(2 * board)}")
 print("Connected!")
 
 # set measurement data using logarithmic spacing
 cal_voltages = np.logspace(-3, 0, 25) * 5
 cal_voltages = [f"{v:6.4f}" for v in cal_voltages]
 
-cal_currents_ = np.logspace(-4, -1, 25) * 2
+cal_currents_ = np.logspace(-4, -2, 25) * 2
 cal_currents_0 = [f"{i:6.4f}" for i in cal_currents_]
 cal_currents_1 = [f"{i:6.4f}" for i in -cal_currents_]
 # for current measurements psu and ADALM1000 see opposite polarities.
@@ -142,36 +152,20 @@ cal_currents_meas = cal_currents_1 + cal_currents_0
 # for ADALM1000 sourcing do the opposite
 cal_currents_source = cal_currents_0 + cal_currents_1
 
-# setup keithley
-print("\nConfiguring Keithley 2400...")
-# reset
-keithley2400.write("*RST")
-# Disable the output
-keithley2400.write(":OUTP OFF")
-# Disable beeper
-keithley2400.write(":SYST:BEEP:STAT OFF")
-# Set front terminals
-keithley2400.write(":ROUT:TERM FRONT")
-# Enable 4-wire sense
-keithley2400.write(":SYST:RSEN 1")
-# Don't auto-off source after measurement
-keithley2400.write(":SOUR:CLE:AUTO OFF")
-# Set output-off mode to high impedance
-keithley2400.write(":OUTP:SMOD HIMP")
-# Make sure output never goes above 20 V
-keithley2400.write(":SOUR:VOLT:PROT 20")
-# Enable and set concurrent measurements
-keithley2400.write(":SENS:FUNC:CONC ON")
-keithley2400.write(':SENS:FUNC "CURR", "VOLT"')
-# Set read format
-keithley2400.write(":FORM:ELEM TIME,VOLT,CURR,STAT")
-# Set the integration filter (applies globally for all measurement types)
-keithley2400.write(f":SENS:CURR:NPLC {nplc}")
-# Set the delay
-keithley2400.write(f":SOUR:DEL {settling_delay}")
-# Disable autozero
-keithley2400.write(":SYST:AZER OFF")
-print("Keithley configuration complete!")
+# setup multimeter
+print("\nConfiguring DMM...")
+dmm.enable_autorange(True)
+print("DMM configuration complete!")
+
+# setup psu
+print("\nConfiguring PSU...")
+# disable output for ch 1
+psu.set_output_enable(False, 1)
+
+# set overcurrent protection for ch 1
+psu.set_ocp_value(0.21, 1)
+psu.set_ocp_enable(True, 1)
+print("PSU configuration complete!")
 
 
 def measure_voltage_cal(smu, channel, save_file, cal_dict):
@@ -193,16 +187,15 @@ def measure_voltage_cal(smu, channel, save_file, cal_dict):
     # get smu sub-channel letter
     dev_channel = smu.channel_settings[channel]["dev_channel"]
 
-    # Autorange keithley source votlage and measure current
-    keithley2400.write(":SOUR:FUNC VOLT")
-    keithley2400.write(':SENS:FUNC "CURR"')
-    keithley2400.write(":SOUR:VOLT:RANG:AUTO ON")
-    keithley2400.write(":SENS:CURR:RANG:AUTO ON")
+    # set dmm to measure voltage
+    dmm.set_function("voltage", "dc")
+    dmm.set_dc_voltage_measurement_impedance("10G")
+    dmm.set_reading_rate("voltage", "dc", "S")
 
-    # set keithley to source zero volts and enable output
-    keithley2400.write(":SOUR:VOLT 0")
-    keithley2400.write(":OUTP ON")
-    keithley2400.write(":SYST:AZER ONCE")
+    # set psu to source zero volts and enable output
+    max_current = 0.01
+    psu.set_apply(channel=1, voltage=0, current=max_current)
+    psu.set_output_enable(True, 1)
 
     # set smu to measure voltage in high impedance mode
     smu.configure_dc(values=0, source_mode="i")
@@ -219,25 +212,24 @@ def measure_voltage_cal(smu, channel, save_file, cal_dict):
         # run through the list of voltages
         cal_ch_meas_v = []
         for v in cal_voltages:
-            keithley2400.write(f":SOUR:VOLT {v}")
+            psu.set_apply(channel=1, voltage=v, current=max_current)
             time.sleep(0.1)
-            keithley_data = keithley2400.query_ascii_values(":READ?")
-            keithley_v = keithley_data[0]
+            dmm_v = dmm.measure("voltage", "dc")
 
             smu_v = smu.measure(measurement="dc")[channel][0][0]
 
-            f.write(f"<{keithley_v:6.4f}, {smu_v:7.5f}>\n")
+            f.write(f"<{dmm_v:6.4f}, {smu_v:7.5f}>\n")
 
-            cal_ch_meas_v.append([keithley_v, smu_v])
-            print(f"Keithley: {keithley_v:6.4f}, SMU: {smu_v:7.5f}")
+            cal_ch_meas_v.append([dmm_v, smu_v])
+            print(f"DMM: {dmm_v:6.4f}, SMU: {smu_v:7.5f}")
 
         f.write("<\>\n\n")
         cal_dict[dev_channel]["meas_v"] = cal_ch_meas_v
 
-    # turn off smu outputs
+    # turn off instrument outputs
     smu.enable_output(False)
-    keithley2400.write(":SOUR:VOLT 0")
-    keithley2400.write(":OUTP OFF")
+    psu.set_apply(channel=1, voltage=0, current=max_current)
+    psu.set_output_enable(False, 1)
 
     print(f"CH{channel + 1} measure voltage calibration measurement complete!")
 
@@ -261,19 +253,17 @@ def measure_current_cal(smu, channel, save_file, cal_dict):
     # get smu sub-channel letter
     dev_channel = smu.channel_settings[channel]["dev_channel"]
 
-    # Autorange keithley source votlage and measure current
-    keithley2400.write(":SOUR:FUNC CURR")
-    keithley2400.write(":SENS:FUNC 'VOLT'")
-    keithley2400.write(":SOUR:CURR:RANG:AUTO ON")
-    keithley2400.write(":SENS:VOLT:RANG:AUTO ON")
+    # setup dmm to measure current
+    dmm.set_function("current", "dc")
+    dmm.set_reading_rate("current", "dc", "S")
 
     # set m1k to source voltage, measure current and set voltage to 0
     smu.configure_dc(values=0, source_mode="v")
 
-    # set keithley to source zero volts and enable output
-    keithley2400.write(":SOUR:CURR 0")
-    keithley2400.write(":OUTP ON")
-    keithley2400.write(":SYST:AZER ONCE")
+    # set psu to source 0 A and enable output
+    max_voltage = 5.1
+    psu.set_apply(channel=1, voltage=max_voltage, current=0)
+    psu.set_output_enable(True, 1)
 
     if save_file.exists() is True:
         write_mode = "a"
@@ -286,27 +276,46 @@ def measure_current_cal(smu, channel, save_file, cal_dict):
         f.write("</>\n")
         # run through the list of voltages
         cal_ch_meas_i = []
-        for i in cal_currents_meas:
-            keithley2400.write(f":SOUR:CURR {i}")
+        for i in cal_currents_0:
+            psu.set_apply(channel=1, voltage=max_voltage, current=i)
             time.sleep(0.1)
-            keithley_data = keithley2400.query_ascii_values(":READ?")
+
             # reverse polarity as SMU's are seeing opposites
-            keithley_i = -keithley_data[1]
+            dmm_i = -dmm.measure("current", "dc")
 
             smu_i = smu.measure(measurement="dc")[channel][0][1]
 
-            f.write(f"<{keithley_i:6.4f}, {smu_i:7.5f}>\n")
+            f.write(f"<{dmm_i:6.4f}, {smu_i:7.5f}>\n")
 
-            cal_ch_meas_i.append([keithley_i, smu_i])
-            print(f"Keithley: {keithley_i:6.4f}, SMU: {smu_i:7.5f}")
+            cal_ch_meas_i.append([dmm_i, smu_i])
+            print(f"DMM: {dmm_i:6.4f}, SMU: {smu_i:7.5f}")
+
+        psu.set_apply(channel=1, voltage=max_voltage, current=0)
+        input(
+            "\nReverse the polarity of the PSU CH1 outputs. Press Enter when ready...\n"
+        )
+
+        for i in cal_currents_1:
+            psu.set_apply(channel=1, voltage=max_voltage, current=i)
+            time.sleep(0.1)
+
+            # reverse polarity as SMU's are seeing opposites
+            dmm_i = -dmm.measure("current", "dc")
+
+            smu_i = smu.measure(measurement="dc")[channel][0][1]
+
+            f.write(f"<{dmm_i:6.4f}, {smu_i:7.5f}>\n")
+
+            cal_ch_meas_i.append([dmm_i, smu_i])
+            print(f"DMM: {dmm_i:6.4f}, SMU: {smu_i:7.5f}")
 
         f.write("<\>\n\n")
         cal_dict[dev_channel]["meas_i"] = cal_ch_meas_i
 
-    # turn off smu outputs
+    # turn off instrument outputs
     smu.enable_output(False)
-    keithley2400.write(":SOUR:CURR 0")
-    keithley2400.write(":OUTP OFF")
+    psu.set_apply(channel=1, voltage=max_voltage, current=0)
+    psu.set_output_enable(False, 1)
 
     print(f"CH{channel + 1} measure current calibration measurement complete!")
 
@@ -330,19 +339,13 @@ def source_voltage_cal(smu, channel, save_file, cal_dict):
     # get smu sub-channel letter
     dev_channel = smu.channel_settings[channel]["dev_channel"]
 
-    # Autorange keithley source votlage and measure current
-    keithley2400.write(":SOUR:FUNC CURR")
-    keithley2400.write(":SENS:FUNC 'VOLT'")
-    keithley2400.write(":SOUR:CURR:RANG:AUTO ON")
-    keithley2400.write(":SENS:VOLT:RANG:AUTO ON")
+    # set dmm to measure voltage
+    dmm.set_function("voltage", "dc")
+    dmm.set_dc_voltage_measurement_impedance("10G")
+    dmm.set_reading_rate("voltage", "dc", "S")
 
     # set smu to source voltage, measure current and set voltage to 0
     smu.configure_dc(values=0, source_mode="v")
-
-    # set keithley to source zero volts and enable output
-    keithley2400.write(":SOUR:CURR 0")
-    keithley2400.write(":OUTP ON")
-    keithley2400.write(":SYST:AZER ONCE")
 
     if save_file.exists() is True:
         write_mode = "a"
@@ -359,21 +362,19 @@ def source_voltage_cal(smu, channel, save_file, cal_dict):
             smu.configure_dc(values=v, source_mode="v")
             time.sleep(0.1)
 
-            keithley_data = keithley2400.query_ascii_values(":READ?")
-            keithley_v = keithley_data[0]
+            dmm_v = dmm.measure("voltage", "dc")
 
             smu_v = smu.measure(measurement="dc")[channel][0][0]
 
-            f.write(f"<{smu_v:7.5f}, {keithley_v:6.4f}>\n")
-            cal_ch_sour_v.append([v, smu_v, keithley_v])
-            print(f"SMU: {smu_v:7.5f}, Keithley: {keithley_v:6.4f}")
+            f.write(f"<{smu_v:7.5f}, {dmm_v:6.4f}>\n")
+            cal_ch_sour_v.append([v, smu_v, dmm_v])
+            print(f"SMU: {smu_v:7.5f}, DMM: {dmm_v:6.4f}")
 
         f.write("<\>\n\n")
         cal_dict[dev_channel]["source_v"] = cal_ch_sour_v
 
     # turn off smu outputs
     smu.enable_output(False)
-    keithley2400.write(":OUTP OFF")
 
     print(f"CH{channel + 1} source voltage calibration measurement complete!")
 
@@ -397,22 +398,12 @@ def source_current_cal(smu, channel, save_file, cal_dict):
     # get smu sub-channel letter
     dev_channel = smu.channel_settings[channel]["dev_channel"]
 
-    # Autorange keithley source votlage and measure current
-    keithley2400.write(":SOUR:FUNC VOLT")
-    keithley2400.write(":SENS:FUNC 'CURR'")
-    keithley2400.write(":SOUR:VOLT:RANG:AUTO ON")
-    keithley2400.write(":SENS:CURR:RANG:AUTO ON")
+    # set dmm to measure current
+    dmm.set_function("current", "dc")
+    dmm.set_reading_rate("current", "dc", "S")
 
     # set smu to source current, measure voltage and set current to 0
     smu.configure_dc(values=0, source_mode="i")
-
-    # set the current compliance
-    keithley2400.write(":SENS:CURR:PROT 0.25")
-
-    # set keithley to source zero volts and enable output
-    keithley2400.write(":SOUR:VOLT 0")
-    keithley2400.write(":OUTP ON")
-    keithley2400.write(":SYST:AZER ONCE")
 
     if save_file.exists() is True:
         write_mode = "a"
@@ -429,24 +420,64 @@ def source_current_cal(smu, channel, save_file, cal_dict):
             smu.configure_dc(values=i, source_mode="i")
             time.sleep(0.1)
 
-            keithley_data = keithley2400.query_ascii_values(":READ?")
             # reverse polarity as SMU's are seeing opposites
-            keithley_i = -keithley_data[1]
+            dmm_i = -dmm.measure("current", "dc")
 
             smu_i = smu.measure(measurement="dc")[channel][0][1]
 
-            f.write(f"<{smu_i:7.5f}, {keithley_i:6.4f}>\n")
-            cal_ch_sour_i.append([i, smu_i, keithley_i])
-            print(f"set: {i}, SMU: {smu_i:6.4f}, Keithley: {keithley_i:7.5f}")
+            f.write(f"<{smu_i:7.5f}, {dmm_i:6.4f}>\n")
+            cal_ch_sour_i.append([i, smu_i, dmm_i])
+            print(f"set: {i}, SMU: {smu_i:6.4f}, DMM: {dmm_i:7.5f}")
 
         f.write("<\>\n\n")
         cal_dict[dev_channel]["source_i"] = cal_ch_sour_i
 
     # turn off smu outputs
     smu.enable_output(False)
-    keithley2400.write(":OUTP OFF")
 
     print(f"CH{channel + 1} source voltage calibration measurement complete!")
+
+
+def channel_cal(smu, channel, save_file, cal_dict):
+    """Run all calibration measurements for a channel.
+
+    Parameters
+    ----------
+    smu : m1k.smu
+        SMU object.
+    channel : int
+        SMU channel number.
+    save_file : str or pathlib.Path
+        Path to save file formatted for internal calibration.
+    cal_dict : dict
+        Calibration dictionary for external calibration.
+    """
+    input(
+        f"\nConnect PSU CH1 HI and DMM HI to SMU CH {channel + 1} HI, and PSU "
+        + f"CH1 LO and DMM LO to SMU CH {channel + 1} LO. Press Enter when "
+        + "ready..."
+    )
+    measure_voltage_cal(smu, channel, save_file, cal_dict)
+
+    input(
+        f"\nConnect DMM HI to SMU CH {channel + 1} HI, and DMM LO to SMU CH "
+        + f"{channel + 1} LO. Press Enter when ready..."
+    )
+    source_voltage_cal(smu, channel, save_file, cal_dict)
+
+    input(
+        f"\nConnect PSU CH1 HI to DMM current HI, DMM LO to SMU CH {channel + 1}"
+        + f" HI, and PSU CH1 LO to SMU CH {channel + 1} LO. Press Enter when "
+        + "ready..."
+    )
+    measure_current_cal(smu, channel, save_file, cal_dict)
+
+    if args.simv is True:
+        input(
+            f"\nConnect DMM current HI to SMU CH {channel + 1} HI and DMM LO to "
+            + f"SMU CH {channel + 1} 2.5 V. Press Enter when ready..."
+        )
+        source_current_cal(smu, channel, save_file, cal_dict)
 
 
 # perform calibration measurements in exact order required for cal file
@@ -468,41 +499,9 @@ for board in range(smu.num_boards):
     channel_A_num = 2 * board
     channel_B_num = 2 * board + 1
 
-    input(
-        "\nConnect Rigol DP821A PSU HI and Rigol DM3058E DMM HI to SMU CH "
-        + f"{channel_A_num + 1} HI, and Rigol DP821A PSU LO and Rigol DM3058E DMM LO"
-        + f" to SMU CH {channel_A_num + 1} LO. Press Enter when ready..."
-    )
-    measure_voltage_cal(smu, channel_A_num, save_file, cal_dict)
-    measure_current_cal(smu, channel_A_num, save_file, cal_dict)
-    source_voltage_cal(smu, channel_A_num, save_file, cal_dict)
-
-    if args.simv is True:
-        input(
-            "\nConnect Rigol DP821A PSU HI and Rigol DM3058E DMM HI to SMU CH "
-            + f"{channel_A_num + 1} HI, and Rigol DP821A PSU LO and Rigol DM3058E "
-            + f"DMM LO to SMU CH {channel_A_num + 1} 2.5 V. Press Enter when "
-            + "ready..."
-        )
-        source_current_cal(smu, channel_A_num, save_file, cal_dict)
-
-    input(
-        "\nConnect Rigol DP821A PSU HI and Rigol DM3058E DMM HI to SMU CH "
-        + f"{channel_B_num + 1} HI, and Rigol DP821A PSU LO and Rigol DM3058E DMM LO"
-        + f" to SMU CH {channel_B_num + 1} LO. Press Enter when ready..."
-    )
-    measure_voltage_cal(smu, channel_B_num, save_file, cal_dict)
-    measure_current_cal(smu, channel_B_num, save_file, cal_dict)
-    source_voltage_cal(smu, channel_B_num, save_file, cal_dict)
-
-    if args.simv is True:
-        input(
-            "\nConnect Rigol DP821A PSU HI and Rigol DM3058E DMM HI to SMU CH "
-            + f"{channel_B_num + 1} HI, and Rigol DP821A PSU LO and Rigol DM3058E "
-            + f"DMM LO to SMU CH {channel_B_num + 1} 2.5 V. Press Enter when "
-            + "ready..."
-        )
-        source_current_cal(smu, channel_B_num, save_file, cal_dict)
+    # run calibrations
+    channel_cal(smu, channel_A_num, save_file, cal_dict)
+    channel_cal(smu, channel_B_num, save_file, cal_dict)
 
     # export calibration dictionary to a yaml file
     with open(save_file_dict, "w") as f:
