75a8d6c42bf3c6980c41fa2c73eca802c036f44d
Greyson Christoforo <grey@christoforo.net>
2021-06-22 09:44:04 +0100

    enable address reuse

--- examples/tcp_server.py
+++ examples/tcp_server.py
@@ -310,6 +310,7 @@ threading.Thread(target=worker, args=(smu,), daemon=True).start()
 
 # start server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
     s.listen()
 
